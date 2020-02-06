6a069844940e9706fb45f7773863b002ae9a6af6
Kilvoctu <kilvoctu@zoho.com>
2020-02-06 16:50:31 -0600

    revert right-aligned "Toggle minimap"

--- src/game.cpp
+++ src/game.cpp
@@ -6751,9 +6751,6 @@ look_around_result game::look_around( const bool show_window, tripoint &center,
                 mvwprintz( w_info, point( 1, getmaxy( w_info ) - 1 ), fast_scroll ? c_light_green : c_green,
                            fast_scroll_text );
 
-                right_print( w_info, getmaxy( w_info ) - 1, 1, pixel_minimap_option ? c_light_green : c_green,
-                             pixel_minimap_text );
-
                 mvwprintz( w_info, point( 1, getmaxy( w_info ) - 2 ),
                            pixel_minimap_option ? c_light_green : c_green, pixel_minimap_text );
 
