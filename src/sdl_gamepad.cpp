8b69d7968984da0070a2ca2bc6a77cbc5d90fae8
Kilvoctu <kilvoctu@zoho.com>
2025-02-06 01:02:52 -0600

    try fix linux build again

--- src/sdl_gamepad.cpp
+++ src/sdl_gamepad.cpp
@@ -289,6 +289,7 @@ void handle_button_event( SDL_Event &event, int inc_keystate )
             } else {
                 cancel_task( task );
             }
+            [[fallthrough]];
         case SDL_CONTROLLERBUTTONUP: {
             if( state ) {
                 switch( button ) {
