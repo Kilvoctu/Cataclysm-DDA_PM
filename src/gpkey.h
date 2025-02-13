5a27dbd4a6d9c1f3755ead11ff2435da5b004ed8
Kilvoctu <kilvoctu@zoho.com>
2025-02-12 19:47:03 -0600

    appease IWYU

--- src/gpkey.h
+++ src/gpkey.h
@@ -2,11 +2,7 @@
 #ifndef GPKEY_H
 #define GPKEY_H
 
-#include <map>
 #include <string>
-#include <utility>
-#include <vector>
-#include <initializer_list>
 
 std::string convert_to_gamepad( const std::string keybind_in );
 std::string convert_joy_string( const std::string joy_in );
